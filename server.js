a9ca20e5ebad4595a075bf5f7547881caa4cbfab
rvdegroen <suiyichiro@hotmail.com>
2022-04-30 23:32:41 +0200

    express config

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 
+//tells express that when I'm using templates, like ejs, it should look for them in the views folder
+app.set('views', './views')
+//tells that while using templates, it should render them using ejs
+app.set('view engine', 'ejs') 
+
+//express knows that now all of my static files are in my static folder
+//to use paths in my html, I can put a / behind the path to folder
+app.use(express.static("static"))
+
+//to render the index.ejs file
 app.get('/', function (req, res) {
-  res.send('Hello Woonoonorld')
+  res.render("pages/index")
 })
 
+//localhost:3000
 app.listen(3000)
