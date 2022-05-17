4c9ce309802c49ae5bf3b08c3c2ee845e29fd6b0
rvdegroen <suiyichiro@hotmail.com>
2022-05-18 01:05:33 +0200

    core functions added that needs some fixing

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ require("dotenv").config();
 
 // VARIABLES
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 // Connection URL to my database
 const url = process.env.DATABASE_URL;
-const client = new MongoClient(url);
-client.connect();
+mongoose.connect(url);
 
 // CONFIGURATION
 // app.set tells express that when I'm using templates, like ejs, it should look for them in the views folder
@@ -18,45 +18,68 @@ app.set("views", "./views");
 // tells that while using templates, it should render them using ejs
 app.set("view engine", "ejs");
 
+// DATABASE CONFIGURATION
+const dishesSchema = new mongoose.Schema({
+  name: String,
+  quality: Number,
+  tags: [String],
+  ingredients: [String],
+  image: String,
+});
+
+// compile the schema into a model - because they're classes you write them with uppercase
+const Dish = mongoose.model("dishes", dishesSchema);
+
 // MIDDLEWARE
 // express knows all of my static files are in my static folder
 app.use(express.static("static"));
-
-// FUNCTIONS
-
-// test function to find one specific dish
-const getDishes = async () => {
-  // open database dishExchange
-  const database = client.db("dishExchange");
-  const dishes = database.collection("dishes");
-
-  // to find one specific dish
-  const query = { name: "Thai green curry" };
-  const dish = await dishes.findOne(query);
-
-  console.log(dish);
-};
+// to be able to use body parser
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
 
 // ROUTES
 
-// 1. homepage to show all dishes
-app.get("/", (req, res) => {
-  res.render("pages/dishes");
+// 1. homepage to show all dishes - NOT DONE
+app.get("/", async (req, res) => {
+  // find is static, because it's a property of the class (dishes) and not an instance (one dish with different values)
+  const dishes = await Dish.find();
+  console.log(dishes);
+
+  res.render("pages/dishes", {
+    dishes,
+  });
 });
 
-// 2. page to add a dish to the database
+// 2. page to add a dish to the database - NOT DONE
 app.get("/add-dish", (req, res) => {
   res.render("pages/addDish");
 });
 
+// 2.1 page to add a dish to the database which then redirects you to the page of that dish
+app.post("/add-dish", async (req, res) => {
+  const newDish = new Dish({ name: req.body.dishName });
+  const dishDatabase = await newDish.save();
+  // eslint-disable-next-line no-underscore-dangle
+  const dishId = dishDatabase._id;
+
+  res.redirect("/dish/" + dishId);
+});
+
 // 3. detail page of the dish (or the dish you might have just added)
-app.get("/dish/:dishId", (req, res) => {
-  res.render("pages/dish");
+app.get("/dish/:dishId", async (req, res) => {
+  const urlId = req.params.dishId;
+  const specificDish = await Dish.findById(urlId);
+  res.render("pages/dish", {
+    specificDish,
+  });
 });
 
 // 4. error 404 page for every route that doesn't exists
 app.get("*", (req, res) => {
-  getDishes();
   res.render("pages/404");
 });
 
