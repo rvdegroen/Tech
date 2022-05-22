56d1b755f9a805ed3af332c08e958ac0c34a54e4
rvdegroen <suiyichiro@hotmail.com>
2022-05-22 16:09:08 +0200

    enabled line for heroku

--- server.js
+++ server.js
@@ -84,5 +84,5 @@ app.get("*", (req, res) => {
 });
 
 // use the port on the env file if it exists, otherwise use port 3000
-// app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
 app.listen(8000);
